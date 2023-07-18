b9b73ebdd28a6cbec33310c123b1ec06d1a733be
srijan-git <srijan.khan20@gmail.com>
2023-07-18 19:09:21 +0530

    Initial Commit

---
+++ Quizzler-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler-App
+//
+//  Created by Srijan Kumar Khan on 18/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
